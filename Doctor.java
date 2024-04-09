77851aad5a18aac1bae3823aa4ef13e41fd7f507
Daniel00Chang <43504251+Daniel00Chang@users.noreply.github.com>
2024-04-09 01:26:17 -0700

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,25 @@
+package cse360;
+
+import java.util.*;
+
+class Doctor{
+	String userName;
+	String password;
+	ArrayList<String> msgsSent;
+	ArrayList<String> msgsReceived;
+	
+	public Doctor(String userNames, String passwords) {
+		userName = userNames;
+		password = passwords;
+	}
+	
+	public String getUser() {return userName;}
+	public String getPass() {return password;}
+	public void sendMsg(String msg, Patient to) {
+		msgsSent.add(msg);
+	}
+	public ArrayList<String> getMsgs() {
+		return msgsReceived;
+	}
+	
+};
