c20cb30fd45a3fa214a31e7011ed7d3d147e1970
Daniel00Chang <43504251+Daniel00Chang@users.noreply.github.com>
2024-04-09 01:25:59 -0700

    Create Nurse.java
    nurses class

---
+++ Nurse.java
@@ -0,0 +1,25 @@
+package cse360;
+
+import java.util.*;
+
+class Nurse{
+	String userName;
+	String password;
+	ArrayList<String> msgsSent;
+	ArrayList<String> msgsReceived;
+	
+	public Nurse(String userNames, String passwords) {
+		userName = userNames;
+		password = passwords;
+	}
+	
+	public String getUser() {return userName;}
+	public String getPass() {return password;}
+	public void sendMsg(String msg, Patient to) {
+		msgsSent.add(msg);
+	}
+	public ArrayList<String> getMsgs() {
+		return msgsReceived;
+	}
+	
+};
