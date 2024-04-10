6840bbb6346749c1ab1c4c88ca9aac3d7ce7c1e5
JoshEbe <145737171+JoshEbe@users.noreply.github.com>
2024-04-09 22:45:33 -0700

    Update VisitNotes.java
    displays info from the file

--- VisitNotes.java
+++ VisitNotes.java
@@ -1,3 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileWriter; 
+import java.io.BufferedWriter;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,6 +16,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class VisitNotes {
@@ -14,11 +24,19 @@ public class VisitNotes {
 	BorderPane mainPane;
 	VBox UIBox, visitBox, historyBox, pastBox;
 	Button homeBtn, msgBtn, saveBtn;
-	Label visitNotes, patientHistory, healthIssues, prescriptions, immunizationHistory, pastVisits, findings, newPrescriptions;
-	TextField findingsField, newPrescripsField, issuesField, prescriptionsField, immunizationField, oldFindingsField, oldPrescripsField;
+	Label visitNotes, patientHistory, pastVisits;
+	TextField findingsField, newPrescripsField;
 	
-	public Scene VisitNotesFunction(Stage primaryStage) {
+	public File patientFile;
+	
+	public ArrayList<PastVisit> pastVisitsList;
+	public History history;
+	
+	public Scene VisitNotesFunction(Stage primaryStage, File patientFile) {
 				
+		pastVisitsList = getPastVisits(patientFile);
+		history = getHistory(patientFile);
+		
 		//panes
 		mainPane = new BorderPane();
 		UIBox = new VBox();
@@ -32,36 +50,21 @@ public class VisitNotes {
 		msgBtn = new Button("msg");
 		msgBtn.setPrefSize(50, 50);
 		saveBtn = new Button("save");
-		saveBtn.setPrefSize(100, 50);
+		saveBtn.setPrefSize(80, 40);
 				
 		//labels
 		visitNotes = new Label("Visit Notes");
 		visitNotes.setFont(Font.font("Arial", FontWeight.BOLD, 16));
 		patientHistory = new Label("Patient History");
 		patientHistory.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-		healthIssues = new Label("Health Issues");
-		healthIssues.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-		prescriptions = new Label("Prescriptions");
-		prescriptions.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-		immunizationHistory = new Label("Immunization History");
-		immunizationHistory.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 		pastVisits = new Label("Past Visits");
 		pastVisits.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-		findings = new Label("Findings");
-		findings.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-		newPrescriptions = new Label("New Prescriptions");
-		newPrescriptions.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 				
 		//textfields
 		findingsField = new TextField();
 		findingsField.setText("Findings");
 		newPrescripsField = new TextField();
 		newPrescripsField.setText("New Prescriptions");
-		issuesField = new TextField();
-		prescriptionsField = new TextField();
-		immunizationField = new TextField();
-		oldFindingsField = new TextField();
-		oldPrescripsField = new TextField();
 				
 		//uibox
 		UIBox.setSpacing(20);
@@ -69,19 +72,38 @@ public class VisitNotes {
 				
 		//historybox
 		historyBox.setStyle("-fx-border-color: black");
-		historyBox.setSpacing(5);
-		historyBox.getChildren().addAll(healthIssues, issuesField, prescriptions, prescriptionsField, 
-				immunizationHistory, immunizationField);
+		historyBox.setSpacing(1);
+		
+		historyBox.getChildren().add(new Text("Health Issues:"));
+		for (int i = 0; i < history.healthIssues.size(); i++) {
+			historyBox.getChildren().addAll(new Text(history.healthIssues.get(i)));
+		}
+		historyBox.getChildren().add(new Text("Prescriptions:"));
+		for (int i = 0; i < history.prescrips.size(); i++) {
+			historyBox.getChildren().addAll(new Text(history.prescrips.get(i)));
+		}
+		historyBox.getChildren().add(new Text("Immunization History:"));
+		for (int i = 0; i < history.immunizations.size(); i++) {
+			historyBox.getChildren().addAll(new Text(history.immunizations.get(i)));
+		}
 				
 		//pastbox
 		pastBox.setStyle("-fx-border-color: black");
-		pastBox.setSpacing(5);
-		pastBox.getChildren().addAll(findings, oldFindingsField, newPrescriptions, oldPrescripsField);
+		pastBox.setSpacing(1);
+		for(int i = 0; i < pastVisitsList.size(); i++) {
+		      PastVisit temp = pastVisitsList.get(i);
+		      pastBox.getChildren().add(new Text("Findings:"));
+		      pastBox.getChildren().add(new Text(temp.findings));
+		      pastBox.getChildren().add(new Text("New prescriptions:"));
+		      for(int j = 0; j < temp.prescriptions.size(); j++) {
+		        pastBox.getChildren().addAll(new Text(temp.prescriptions.get(j)));
+		      }
+		    }
 				
 		//visitBox
 		visitBox.setSpacing(5);
-		visitBox.getChildren().addAll(visitNotes, findingsField, newPrescripsField, patientHistory,
-				historyBox, pastVisits, pastBox, saveBtn);
+		visitBox.getChildren().addAll(visitNotes, findingsField, newPrescripsField, saveBtn, patientHistory,
+				historyBox, pastVisits, pastBox);
 				
 		mainPane.setLeft(UIBox);
 		mainPane.setCenter(visitBox);
@@ -89,8 +111,125 @@ public class VisitNotes {
 		BorderPane.setMargin(UIBox, new Insets(0, 10, 0, 0));
 		BorderPane.setMargin(visitBox, new Insets(0, 10, 10, 0));
 			
+		homeBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				//take to doctor view
+				
+				
+			}
+		});
+		
+		msgBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				//take to messagesND
+				
+				
+			}
+		});
+		
+		
+		saveBtn.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				//save
+				
+			}
+		});
+		
+		
 		Scene visitNotesScene = new Scene(mainPane, 700, 500);
 		return visitNotesScene;
 		
 	}
+	
+	public ArrayList<PastVisit> getPastVisits(File f){
+	    int amountOfPastVisits = 0;
+	    PastVisit temp;
+	    ArrayList<PastVisit> pastVisits = new ArrayList<PastVisit>();
+	    try {
+	      FileReader fr = new FileReader(f);
+	      BufferedReader br = new BufferedReader(fr);
+	      String check = "1";
+	      //goes down to past visit in file
+	      while(!check.equals("Past Visits")) {
+	        check = br.readLine();
+	      }
+
+	      while(!check.equals("past visits done")) {
+	        while(!check.equals("Findings:")) {
+	          check = br.readLine();
+	        }
+	        pastVisits.add(new PastVisit());
+	        temp = pastVisits.get(amountOfPastVisits);
+	        temp.findings = br.readLine();
+	        
+	        while(!check.equals("New prescriptions:")) {
+	          check = br.readLine();
+	        }
+	        while(!check.equals("")) {
+	          check = br.readLine();
+	          temp.prescriptions.add(check);
+	        }
+	        check = br.readLine();
+	        amountOfPastVisits++;
+	      }
+	      br.close();
+	      
+	    } catch (IOException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	    } 
+
+	    return pastVisits;
+	  }
+	
+	public History getHistory(File f) {
+		History hist = new History();
+		try {
+			FileReader fr = new FileReader(f);
+		    BufferedReader br = new BufferedReader(fr);
+		    String check = "1";
+		    //goes down to history in file
+		    while(!check.equals("History")) {
+		    	check = br.readLine();
+		    }
+		    
+		    while (!check.equals("Past Visits")) {
+		    	while (!check.equals("Health Issue:")) {
+		    		check = br.readLine();
+		    	}
+		    	hist.addIssues(br.readLine());
+		    	check = br.readLine();
+		    	
+		    	while (!check.equals("Precriptions:")) {
+		    		check = br.readLine();
+		    	}
+		    	
+		    	while (!check.equals("")) {
+		    		check = br.readLine();
+		    		hist.addPrescrip(check);
+		    	}
+		    	
+		    	while (!check.equals("Immunization History:")) {
+		    		check = br.readLine();
+		    	}
+		    	
+		    	check = br.readLine();
+		    	
+		    	while (!check.equals("")) {
+		    		check = br.readLine();
+		    		hist.addImmunizations(check);
+		    	}
+		    	
+		    	check = br.readLine();
+		 
+		    }
+		    br.close();
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		     e.printStackTrace();
+		}
+		return hist;
+	}	
+	
 }
