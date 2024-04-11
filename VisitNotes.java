99e4e0541378cc3564eaeb8ce1a6429c181a0329
JoshEbe <145737171+JoshEbe@users.noreply.github.com>
2024-04-10 18:12:36 -0700

    Update VisitNotes.java

--- VisitNotes.java
+++ VisitNotes.java
@@ -31,7 +31,6 @@ public class VisitNotes {
 	public Patient patient;
 	
 	public ArrayList<PastVisit> pastVisitsList;
-	//public History history;
 	public String history;
 	
 	private DatabaseManager db;
@@ -39,7 +38,6 @@ public class VisitNotes {
 	public Scene VisitNotesFunction(Stage primaryStage, File patientFile) {
 				
 		db = new DatabaseManager();
-		//String path = ("./src/patientDB/" + patientFile.getName());
 		String path = ("" + System.getProperty("user.dir") + "/src/patientDB/jb1.txt");
 		patient = db.readPatientFile(path);
 
